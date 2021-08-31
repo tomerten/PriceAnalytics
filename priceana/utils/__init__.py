cd21c258a8320b3186ae85aa2893e8d23972f44f
Tom Mertens <tommertensjobapplication@gmail.com>
2021-08-31 15:18:03 +0200

    loaded urlutils module in init of utils

--- priceana/utils/__init__.py
+++ priceana/utils/__init__.py
@@ -11,3 +11,10 @@ A module containing various tools used in the package.
 """
 
 from .DateTimeUtils import clean_start_end_period, validate_date
+from .UrlUtils import (
+    generate_combinations,
+    generate_price_params,
+    generate_price_urls,
+    generate_yahoo_financial_data_params,
+    generate_yahoo_financial_data_urls,
+)
