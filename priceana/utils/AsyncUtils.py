38e86b415d976b73ceb4f92fa1ad13ad7e87e040
Tom Mertens <tommertensjobapplication@gmail.com>
2021-09-02 14:48:27 +0200

    started adding async getting, parsing, saving methods

--- priceana/utils/AsyncUtils.py
+++ priceana/utils/AsyncUtils.py
@@ -22,8 +22,9 @@ from aiohttp.http import HttpProcessingError
 from pymongo import ASCENDING
 from termcolor import colored
 
+from .DataBroker import DataBrokerMongoDb
 from .LoggingUtils import logger
-from .ParseUtils import parse_prices
+from .ParseUtils import generate_database_indices_dict, parse_prices, parse_to_multiindex
 
 
 async def fetch(url: str, params: dict, session: ClientSession) -> dict:
@@ -130,3 +131,155 @@ async def aparse_yahoo_prices(
         # )
         logger.exception(colored(f"{tup[0].split('/')[-1]}", "red"))
         return None, None, None, None
+
+
+async def aparse_raw_yahoo_financial_data(
+    sem: Semaphore, tup: Tuple[str, dict], session: ClientSession
+) -> dict:
+    """Method to async get and parse yahoo raw financial data.
+
+    Args:
+        sem (Semaphore): semaphore
+        tup (Tuple[str, dict]): (url, param])
+        session (ClientSession): asynch ClientSession instance
+
+    Returns:
+        dict: key is data info and values are the actual data
+    """
+    try:
+        url, params = tup
+        resp = await bound_fetch(sem, url, params, session)
+        resp = resp["quoteSummary"]["result"][0]
+        cleaned_resp = parse_raw_fmt(resp)
+        logger.debug(f"Cleaning done for {tup[0].split('/')[-1]}")
+        return cleaned_resp
+
+    except (
+        ClientError,
+        HttpProcessingError,
+    ) as e:
+        logger.error(
+            "aiohttp exception for %s [%s]: %s",
+            tup[0],
+            getattr(e, "status", None),
+            getattr(e, "message", None),
+        )
+        return {}
+    except Exception as e:
+        logger.exception("Non-aiohttp exception occured:  %s", getattr(e, "__dict__", {}))
+        return {}
+
+
+async def aparse_multiindex_yahoo_financial_data(
+    sem: Semaphore, tup: Tuple[str, dict], session: ClientSession
+) -> Generator:
+    try:
+        data = await aparse_raw_yahoo_financial_data(sem, tup, session)
+        transformed_financial_data = parse_to_multiindex(data)
+        logger.debug(f"Multi-indexing done for {tup[0].split('/')[-1]}")
+        return transformed_financial_data
+
+    except Exception as e:
+        logger.exception("Non-aiohttp exception occured:  %s", getattr(e, "__dict__", {}))
+        return ({} for i in range(0))
+
+
+async def aparse_yahoo_financial_data(
+    sem: Semaphore, tup: Tuple[str, dict], session: ClientSession
+) -> dict:
+    # FINANCIAL DATA NOT CONTAINING SOME KIND
+    # OF DATE REFERENCE NEED MANUAL ADDING OF DATE
+    DATEUPDATELIST = [
+        "assetProfile",
+        "recommendationTrend_trend",
+        "indexTrend_estimates",
+        "indexTrend",
+        "defaultKeyStatistics",
+        "summaryDetail",
+        "calendarEvents_earnings",
+        "price",
+        "earningsTrend_trend_earningsEstimate",
+        "earningsTrend_trend_revenueEstimate",
+        "earningsTrend_trend_epsTrend",
+        "earningsTrend_trend_epsRevisions",
+        "earningsTrend_trend",
+        "majorHoldersBreakdown",
+        "earningsHistory_history",
+        "netSharePurchaseActivity",
+        "insiderTransactions_transactions",
+        "financialData",
+        "quoteType",
+        "calendarEvents",
+        "esgScores_peerEsgScorePerformance",
+        "esgScores_peerEnvironmentPerformance",
+        "esgScores_peerEsgScorePerformance",
+        "esgScores_peerGovernancePerformance",
+        "esgScores_peerHighestControversyPerformance",
+        "esgScores_peerSocialPerformance",
+        "majorDirectHolders_holders",
+    ]
+    try:
+        tf = await aparse_multiindex_yahoo_financial_data(sem, tup, session)
+        tfd = list(tf)
+
+        _keys = [k for k, g in itertools.groupby(tfd, lambda x: list(x.keys())[0])]
+        cglist = [
+            list(map(lambda d: list(d.values())[0], list(g)))
+            for k, g in itertools.groupby(tfd, lambda x: list(x.keys())[0])
+        ]
+        dc = dict(zip(_keys, cglist))
+
+        dc = {
+            k: v for k, v in dc.items() if v not in [[None], [{"maxAge": 86400}], [{"maxAge": 1}]]
+        }
+        dfsdc = {"_".join(k): pd.DataFrame(v) for k, v in dc.items()}
+        date = dt.fromtimestamp(time.mktime(dt.today().date().timetuple()))
+
+        current_symbol = tup[0].split("/")[-1]
+
+        for k, v in dfsdc.items():
+            if k in DATEUPDATELIST:
+                v["date"] = date.strftime("%Y-%m-%d")
+                if k == "majorHoldersBreakdown":
+                    v["reportDate"] = date.strftime("%Y-%m-%d")
+
+            if k != "quoteType":
+                v["symbol"] = current_symbol
+
+        data = {k: v.to_dict(orient="records") for k, v in dfsdc.items()}
+
+        logger.info(f"Processing financial data {current_symbol} - done!")
+
+        return data
+
+    except Exception as e:
+        logger.exception("Non-aiohttp exception occured:  %s", getattr(e, "__dict__", {}))
+        return {}
+
+
+async def store_yahoo_financial_data(
+    sem: Semaphore,
+    tup: Tuple[str, dict],
+    session: ClientSession,
+    databroker: DataBrokerMongoDb,
+    dbname: str = "FinData",
+):
+    findata = await aparse_yahoo_financial_data(sem, tup, session)
+    indexdict = generate_database_indices_dict(findata)
+
+    newindexdict = {}
+    if isinstance(databroker, DataBrokerMongoDb):
+        for k, v in indexdict.items():
+            newindexdict[k] = [(vv, ASCENDING) for vv in v]
+    else:
+        newindexdict = indexdict.copy()
+
+    # logger.info(f"Findata : {findata}")
+    # logger.info(f"indexdict: {newindexdict}")
+
+    for k, v in findata.items():
+        # logger.info(k, newindexdict[k])
+        # vv = deepcopy(v)  # otherwise the original dict (self._yh_finjson) is updated with _id field
+        databroker.save(v, dbname, k, indexTupleList=newindexdict[k], unique=True)
+
+    logger.info(colored(f'Saving {tup[0].split("/")[-1]} yahoo financials done !', "green"))
