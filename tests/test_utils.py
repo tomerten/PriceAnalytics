b9a8fcb8373ceef89ae7e9aebc60402ef58a3542
Tom Mertens <tommertensjobapplication@gmail.com>
2021-09-02 14:22:52 +0200

    added unit tests for parseutils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,10 +6,20 @@ import itertools
 import time
 from datetime import datetime as dt
 
-import priceana.utils
+import numpy as np
+import pandas as pd
 import pytest
+from pandas.testing import assert_frame_equal
 from priceana.constants import base_url, query_url
 from priceana.utils.DateTimeUtils import clean_start_end_period, validate_date
+from priceana.utils.ParseUtils import (
+    generate_database_indices_dict,
+    parse_from_multiindex,
+    parse_prices,
+    parse_quotes_as_frame,
+    parse_raw_fmt,
+    parse_to_multiindex,
+)
 from priceana.utils.UrlUtils import (
     generate_combinations,
     generate_price_params,
@@ -433,6 +443,349 @@ def test___validate_date___fail():
 def test___validate_date___typeerror___fail():
     with raises(TypeError):
         validate_date({})
+
+
+################################################################################
+# TESTS FOR PARSEUTILS
+################################################################################
+
+
+test_parse_raw_fmt_pass = [
+    ({"test": {"raw": 100, "fmt": "1k"}}, {"test": 100.0}),
+    ({"date": {"raw": 100, "fmt": "2020-01-01"}}, {"date": "2020-01-01"}),
+    (
+        {"lastfiscalYearEnd": {"raw": 100, "fmt": "2020-01-01"}},
+        {"lastfiscalYearEnd": "2020-01-01"},
+    ),
+    (
+        {"nextfiscalYearEnd": {"raw": 100, "fmt": "2020-01-01"}},
+        {"nextfiscalYearEnd": "2020-01-01"},
+    ),
+    (
+        {"mostrecentQuarter": {"raw": 100, "fmt": "2020-01-01"}},
+        {"mostrecentQuarter": "2020-01-01"},
+    ),
+    ({"testpercent": {"raw": 100, "fmt": "100%"}}, {"testpercent": 100.0}),
+    ({"nexttest": {"test": {"raw": 100, "fmt": "1k"}}}, {"nexttest": {"test": 100.0}}),
+    (
+        {"nexttest": [{"test": {"raw": 100, "fmt": "1k"}}]},
+        {"nexttest": [{"test": 100.0}]},
+    ),
+    (
+        {
+            "nexttest": [
+                {"test": {"raw": 100, "fmt": "1k"}},
+                {"test2": {"raw": 10, "fmt": "1k"}},
+            ]
+        },
+        {"nexttest": [{"test": 100.0}, {"test2": 10.0}]},
+    ),
+    ({}, {}),
+    (None, {}),
+    ({"test": 1}, {"test": 1}),
+    (
+        {"nexttest": [{"test": 1}, {"test2": {"raw": 1000, "fmt": "1k"}}]},
+        {"nexttest": [{"test": 1}, {"test2": 1000.0}]},
+    ),
+]
+
+test_parse_multiindex_pass = [
+    ({}, [], {}),
+    (
+        {"test": 100, "test2": 1},
+        [{(): {"test": 100, "test2": 1}}],
+        {"": [{"test": 100, "test2": 1, "symbol": "abc"}]},
+    ),
+    (
+        {"test": 100, "test2": {"dat0": 1, "dat1": 2}},
+        [{("test2",): {"dat0": 1, "dat1": 2}}, {(): {"test": 100}}],
+        {
+            "test2": [{"dat0": 1, "dat1": 2, "symbol": "abc"}],
+            "": [{"test": 100, "symbol": "abc"}],
+        },
+    ),
+    (
+        {"test": {"data": 100}},
+        [{("test",): {"data": 100.0}}],
+        {"test": [{"data": 100, "symbol": "abc"}]},
+    ),
+    (
+        {"test2": {"test": {"data": 100}}},
+        [{("test2", "test"): {"data": 100.0}}],
+        {"test2_test": [{"data": 100, "symbol": "abc"}]},
+    ),
+    (None, [], {}),
+    (
+        [1, 2],
+        [{(): 1}, {(): 2}],
+        {"": [{0: 1, "symbol": "abc"}, {0: 2, "symbol": "abc"}]},
+    ),
+    (
+        [{"a": 1}, {"b": 2}],
+        [{(): {"a": 1}}, {(): {"b": 2}}],
+        {
+            "": [
+                {"a": 1.0, "b": 0.0, "symbol": "abc"},
+                {"a": 0.0, "b": 2.0, "symbol": "abc"},
+            ]
+        },
+    ),
+    (
+        {"test": [1, 2]},
+        [{("test",): 1}, {("test",): 2}],
+        {"test": [{0: 1, "symbol": "abc"}, {0: 2, "symbol": "abc"}]},
+    ),
+    (
+        {"test": [{"a": 1}, {"b": 2}]},
+        [{("test",): {"a": 1}}, {("test",): {"b": 2}}],
+        {
+            "test": [
+                {"a": 1.0, "b": 0.0, "symbol": "abc"},
+                {"a": 0.0, "b": 2.0, "symbol": "abc"},
+            ]
+        },
+    ),
+]
+
+empty_quote_frame = pd.DataFrame(columns=["open", "high", "low", "close", "adjclose", "volume"])
+test_parse_prices_pass: list = [
+    (None, None, None, None, None),
+    ({}, None, empty_quote_frame, None, None),
+    (
+        {
+            "meta": {
+                "symbol": "abc",
+                "exchangeName": "F",
+                "priceHint": 2,
+                "dataGranularity": "1d",
+                "currency": "USD",
+            }
+        },
+        "1d",
+        empty_quote_frame,
+        None,
+        None,
+    ),
+    (
+        {
+            "meta": {
+                "symbol": "abc",
+                "exchangeName": "F",
+                "priceHint": 2,
+                "dataGranularity": "1d",
+                "currency": "USD",
+            },
+            "timestamp": [],
+        },
+        "1d",
+        empty_quote_frame,
+        None,
+        None,
+    ),
+    (
+        {
+            "meta": {
+                "symbol": "abc",
+                "exchangeName": "F",
+                "priceHint": 2,
+                "dataGranularity": "1d",
+                "currency": "USD",
+            },
+            "timestamp": [1, 2],
+        },
+        "1d",
+        empty_quote_frame,
+        None,
+        None,
+    ),
+    (
+        {
+            "meta": {
+                "symbol": "abc",
+                "exchangeName": "F",
+                "priceHint": 2,
+                "dataGranularity": "1d",
+                "currency": "USD",
+            },
+            "timestamp": [1, 2],
+            "indicators": {"quote": None},
+        },
+        "1d",
+        empty_quote_frame,
+        None,
+        None,
+    ),
+    (
+        {
+            "meta": {
+                "symbol": "abc",
+                "exchangeName": "F",
+                "priceHint": 2,
+                "dataGranularity": "1d",
+                "currency": "USD",
+            },
+            "timestamp": [1, 2],
+            "indicators": {"quote": {}},
+        },
+        "1d",
+        empty_quote_frame,
+        None,
+        None,
+    ),
+    (
+        {
+            "meta": {
+                "symbol": "abc",
+                "exchangeName": "F",
+                "priceHint": 2,
+                "dataGranularity": "1d",
+                "currency": "USD",
+            },
+            "timestamp": [1, 2],
+            "indicators": {"quote": 12},
+        },
+        "1d",
+        empty_quote_frame,
+        None,
+        None,
+    ),
+]
+
+test_generate_database_indices_dict___pass: list = [
+    (None, {}),
+    ({}, {}),
+    ({"test": None}, {}),
+    ({"test": []}, {}),
+    (
+        {"quoteType": [{"symbol": "abc", "date": "2020-01-01", "val": 1}]},
+        {"quoteType": ["symbol", "date"]},
+    ),
+    (
+        {
+            "quoteType": [
+                {"symbol": "abc", "date": "2020-01-01", "val": 1},
+                {"symbol": "abc", "date": "2020-01-02", "val": 1},
+                {"symbol": "abc", "date": "2020-01-03", "val": 1},
+            ]
+        },
+        {"quoteType": ["symbol", "date"]},
+    ),
+    (
+        {
+            "balanceSheetHistoryQuarterly_balanceSheetStatements": [
+                {"symbol": "abc", "endDate": "2020-01-01", "val": 1},
+                {"symbol": "abc", "endDate": "2020-01-02", "val": 1},
+                {"symbol": "abc", "endDate": "2020-01-03", "val": 1},
+            ]
+        },
+        {"balanceSheetHistoryQuarterly_balanceSheetStatements": ["symbol", "endDate"]},
+    ),
+    (
+        {
+            "assetProfile_companyOfficers": [
+                {"symbol": "abc", "fiscalYear": "2020", "name": "X", "val": 1},
+                {"symbol": "abc", "fiscalYear": "2020", "name": "Y", "val": 1},
+                {"symbol": "abc", "fiscalYear": "2020", "name": "Z", "val": 1},
+            ]
+        },
+        {"assetProfile_companyOfficers": ["symbol", "fiscalYear", "name"]},
+    ),
+    (
+        {
+            "fundOwnership_ownershipList": [
+                {
+                    "symbol": "abc",
+                    "reportDate": "2020-01-01",
+                    "organization": "org1",
+                    "val": 1,
+                },
+                {
+                    "symbol": "abc",
+                    "reportDate": "2020-01-02",
+                    "organization": "org1",
+                    "val": 1,
+                },
+                {
+                    "symbol": "abc",
+                    "reportDate": "2020-01-03",
+                    "organization": "org2",
+                    "val": 1,
+                },
+            ]
+        },
+        {"fundOwnership_ownershipList": ["symbol", "reportDate", "organization"]},
+    ),
+]
+
+
+@pytest.mark.parametrize("datadc,expected", test_parse_raw_fmt_pass)
+def test___parse_raw_fmt___pass(datadc, expected):
+    assert expected == parse_raw_fmt(datadc)
+
+
+@pytest.mark.parametrize("dc,expected,expected2", test_parse_multiindex_pass)
+def test___parse_to_multiindex___pass(dc, expected, expected2):
+    assert expected == list(parse_to_multiindex(dc))
+
+
+@pytest.mark.parametrize("dc,interval,quotes,div,split", test_parse_prices_pass)
+def test___parse_prices___pass(dc, interval, quotes, div, split):
+    i, q, d, s = parse_prices(dc)
+
+    assert interval == i
+    if q is not None:
+        assert_frame_equal(q, quotes)
+    else:
+        assert q is None
+    assert div == d
+    assert split == s
+
+
+@pytest.mark.parametrize("dc,expected, expected2", test_parse_multiindex_pass)
+def test___parse_from_multiindex___pass(dc, expected, expected2):
+    assert expected == list(parse_to_multiindex(dc))
+    print(parse_from_multiindex("abc", parse_to_multiindex(dc)))
+    actual = parse_from_multiindex("abc", parse_to_multiindex(dc))
+    if "test" in actual.keys() and "b" in actual["test"][0].keys():
+        print(type(actual["test"][0]["b"]))
+        print(type(expected2["test"][0]["b"]))
+        print((actual["test"][0]["b"]))
+        print((expected2["test"][0]["b"]))
+        print(np.isnan(expected2["test"][0]["b"]))
+    assert expected2 == parse_from_multiindex("abc", parse_to_multiindex(dc))
+
+
+@pytest.mark.parametrize("dc,expected", test_generate_database_indices_dict___pass)
+def test___generate_database_indices_dict___pass(dc, expected):
+    assert expected == generate_database_indices_dict(dc)
+
+
+#
+# def test___generate_db_indices_reportDate_organization___pass():
+#     data = {
+#         "test": [
+#             {
+#                 "symbol": "abc",
+#                 "reportDate": "2020-01-01",
+#                 "organization": "org1",
+#                 "val": 1,
+#             },
+#             {
+#                 "symbol": "abc",
+#                 "reportDate": "2020-01-02",
+#                 "organization": "org1",
+#                 "val": 1,
+#             },
+#             {
+#                 "symbol": "abc",
+#                 "reportDate": "2020-01-03",
+#                 "organization": "org2",
+#                 "val": 1,
+#             },
+#         ]
+#     }
+#     idx = yd._generate_db_indices(data)
+#     assert idx == {"test": [("symbol", 1), ("reportDate", 1), ("organization", 1)]}
 
 
 # ==============================================================================
