cca00de01105c5857355543f38fd5f048a7e56dd
Tom Mertens <tommertensjobapplication@gmail.com>
2021-09-05 17:24:55 +0200

    added error classes to main

--- priceana/__init__.py
+++ priceana/__init__.py
@@ -29,4 +29,35 @@ from .constants import (
     weekly_keys,
     yearly_keys,
 )
+from .utils.AsyncUtils import aparse_yahoo_prices, store_yahoo_financial_data, store_yahoo_prices
 from .utils.DataBroker import DataBrokerMongoDb
+from .utils.LoggingUtils import logger
+from .utils.UrlUtils import generate_combinations, generate_price_params, generate_price_urls
+
+
+class InvalidPeriodError(Exception):
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return "InvalidPeriodError, {0} ".format(self.message)
+        else:
+            return "InvalidPeriodError: Invalid period for price time-series."
+
+
+class InvalidIntervalError(Exception):
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return "InvalidIntervalError, {0} ".format(self.message)
+        else:
+            return "InvalidIntervalError: Invalid interval for price time-series."
