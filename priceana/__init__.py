3f8cd473953be0ea877954539b00fdf2c37e7dad
Tom Mertens <tommertensjobapplication@gmail.com>
2021-09-05 09:29:44 +0200

    started adding main async core

--- priceana/__init__.py
+++ priceana/__init__.py
@@ -9,14 +9,24 @@ Top-level package for priceana.
 
 __version__ = "0.0.0"
 
+import asyncio
+import time
+from asyncio import Semaphore
+from collections import namedtuple
+from datetime import timedelta
+from typing import List, Tuple
 
-def hello(who='world'):
-    """'Hello world' method.
+from aiohttp import ClientSession
+from termcolor import colored
 
-    :param str who: whom to say hello to
-    :returns: a string
-    """
-    result = "Hello " + who
-    return result
-
-# Your code here...
+from .constants import (
+    all_keys,
+    daily_keys,
+    monthly_keys,
+    quarterly_keys,
+    valid_intervals,
+    valid_periods,
+    weekly_keys,
+    yearly_keys,
+)
+from .utils.DataBroker import DataBrokerMongoDb
